d0a68dc552d7dad24b972292cbf338b1b54ca5fe
Rishu Anand <riuandg5@gmail.com>
2023-09-06 13:14:18 +0530

    feat: add worker and workerParams to GroupTask
    GroupTask can have worker and workerParams, which will be passed to all subTasks as a fallback config.

--- src/GroupTask/GroupTask.ts
+++ src/GroupTask/GroupTask.ts
@@ -2,7 +2,12 @@ import depd from "depd";
 const deprecate = depd("@riuandg5/task-excute");
 
 import { Task } from "../Task/Task.js";
-import type { TaskConfigI, TaskResultT } from "../Task/Task.js";
+import type {
+    TaskWorkerT,
+    TaskWorkerParamsT,
+    TaskConfigI,
+    TaskResultT,
+} from "../Task/Task.js";
 
 export type GroupTaskModeT = "series" | "parallel";
 /**
@@ -16,14 +21,16 @@ export type GroupSubTasksT<
     R,
 > = ArrayWithMinOneOrTwoElementsT<Task<T, R> | GroupTask<T, R>>;
 
-interface GroupTaskConfigOldI<T extends unknown[], R> {
+interface GroupTaskConfigOldI<T extends unknown[], R>
+    extends TaskConfigI<T, R> {
     /**
      * @deprecated Will be removed in next major release. Use mode instead of type.
      */
     type: GroupTaskModeT;
     subTasks: GroupSubTasksT<T, R>;
 }
-interface GroupTaskConfigNewI<T extends unknown[], R> {
+interface GroupTaskConfigNewI<T extends unknown[], R>
+    extends TaskConfigI<T, R> {
     mode: GroupTaskModeT;
     subTasks: GroupSubTasksT<T, R>;
 }
@@ -35,6 +42,8 @@ type NestedResultT<R> = (R | NestedResultT<R>)[];
 export type GroupTaskResultT<R> = NestedResultT<TaskResultT<R>> | undefined;
 
 export class GroupTask<T extends unknown[], R> {
+    #worker;
+    #workerParams;
     #mode;
     #subTasks;
     #result: TaskResultT<R> | GroupTaskResultT<R>;
@@ -44,7 +53,8 @@ export class GroupTask<T extends unknown[], R> {
             throw new Error("cannot create grouptask without config");
         }
 
-        const { type, subTasks } = groupTaskConfig as GroupTaskConfigOldI<T, R>;
+        const { type, subTasks, worker, workerParams } =
+            groupTaskConfig as GroupTaskConfigOldI<T, R>;
         let { mode } = groupTaskConfig as GroupTaskConfigNewI<T, R>;
 
         if (type) {
@@ -68,8 +78,18 @@ export class GroupTask<T extends unknown[], R> {
             );
         }
 
+        this.#worker = worker;
+        this.#workerParams = workerParams;
         this.#mode = mode;
         this.#subTasks = subTasks;
+    }
+
+    get worker() {
+        return this.#worker;
+    }
+
+    get workerParams() {
+        return this.#workerParams;
     }
 
     get type() {
@@ -90,18 +110,36 @@ export class GroupTask<T extends unknown[], R> {
         return this.#result;
     }
 
+    set worker(worker: TaskWorkerT<T, R>) {
+        this.#worker = worker;
+        this.#result = undefined;
+    }
+
+    set workerParams(workerParams: TaskWorkerParamsT<T>) {
+        this.#workerParams = workerParams;
+        this.#result = undefined;
+    }
+
     async execute(
         fbConfig: TaskConfigI<T, R> = {},
     ): Promise<GroupTaskResultT<R>> {
+        const { worker: fbWorker, workerParams: fbWorkerParams } = fbConfig;
+
+        const worker = this.worker || fbWorker;
+        const workerParams = this.workerParams || fbWorkerParams;
+
+        const newFbConfig: TaskConfigI<T, R> = { worker, workerParams };
+
         let result: GroupTaskResultT<R> = [];
+
         if (this.mode === "series") {
             for (const subTask of this.subTasks) {
-                const res = await subTask.execute(fbConfig);
+                const res = await subTask.execute(newFbConfig);
                 result.push(res);
             }
         } else if (this.mode === "parallel") {
             result = await Promise.all(
-                this.#subTasks.map((subTask) => subTask.execute(fbConfig)),
+                this.#subTasks.map((subTask) => subTask.execute(newFbConfig)),
             );
         }
         this.#result = result;
